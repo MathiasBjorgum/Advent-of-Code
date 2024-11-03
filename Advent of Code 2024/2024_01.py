4db6b09c8ef73159c633d07b5038ba155c02f444
MathiasBjorgum <mathias.bjorgum@gmail.com>
2024-11-03 14:59:28 +0100

    2024 prep

---
+++ Advent of Code 2024/2024_01.py
@@ -0,0 +1,5 @@
+from data.data_util import get_data
+
+
+data = get_data()
+print(data)
---
+++ Advent of Code 2024/data/data_util.py
@@ -0,0 +1,28 @@
+import os
+from inspect import stack
+from pathlib import Path
+
+DATA_PATH = Path(os.path.dirname(__file__))
+
+
+def get_data(test_data: bool = False):
+
+    filename = os.path.splitext(os.path.basename(stack()[1].filename))[0]
+
+    if test_data:
+        filename = filename + "_test"
+
+    filename = filename + ".txt"
+
+    with open(DATA_PATH / 'data' / filename, "r") as f:
+        return f.read()
+
+def create_text_files():
+    os.chdir(DATA_PATH)
+
+    for i in range(2,26):
+        print(str(i).zfill(2))
+        with open(f"2022_{str(i).zfill(2)}_test.txt", "w") as f:
+            pass
+        with open(f"2022_{str(i).zfill(2)}.txt", "w") as f:
+            pass
